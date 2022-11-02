6b0f1f3f3b5da67c402b65bf940897881bd3f4e4
navya ps <navyaps2k14@gmail.com>
2022-11-03 00:10:46 +0530

    navbar home basics

---
+++ fitness/src/About.js
@@ -0,0 +1,7 @@
+const About = () => {
+    return ( 
+        <h1>About sample</h1>
+     );
+}
+ 
+export default About;
---
+++ fitness/src/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    
+    return ( 
+        <nav>
+            <div>
+            <h2>Fitness Tracker</h2>
+            <a href ="/about">About</a>
+            <a href="/profile">Profile</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default NavBar;
